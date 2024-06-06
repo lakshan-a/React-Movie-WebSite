e02afc211155bceacb0f88c870c89126a47441be
lakshan <lakshanrashmika16302@gmail.com>
2024-06-06 23:42:56 +0530

    updated

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,6 +3,7 @@ import './header.css';
 import NavListItem from '../components/NavListItem'
 import navListData from '../data/navListData'
 import Search from '../components/Search';
+import Button from '../components/Button';
 
 function Header() {
   return (
@@ -16,6 +17,10 @@ function Header() {
             }
         </ul>
         <Search/>
+        <Button 
+          icon={<ion-icon name="log-in-outline"></ion-icon>} 
+          name='Sign in'
+        />
     </header>
   )
 }
