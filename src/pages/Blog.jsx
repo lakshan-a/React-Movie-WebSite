9ead8907dc80b97c634141a6065b14b728d338e5
lakshan <lakshanrashmika16302@gmail.com>
2024-06-13 19:50:09 +0530

    updated

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './blog.css';
+import BlogCard from '../components/BlogCard';
 
 function Blog() {
     const [blogs, setBlogs] = useState([]);
@@ -7,7 +8,9 @@ function Blog() {
     const fetchData = () => {
         fetch('http://localhost:3000/data/movieData.json')
         .then(res => res.json())
-        .then(data => setBlogs(data))
+        .then(data => {
+            setBlogs(data);
+        })
         .catch(e => console.log(e.message));
     };
 
@@ -22,7 +25,11 @@ function Blog() {
                 <h4 className="section-title">Our Blog</h4>
             </div>
             <div className="row mt-5">
-
+                {
+                    blogs && blogs.length > 0 && blogs.map(blog => {
+                        <BlogCard key={blog._id} blog={blog}/>
+                    })
+                }
             </div>
         </div>
     </section>
